e838798d15507b13f7dce7727d42251cdcd1d3e0
Philiks <janopol.felixjr@gmail.com>
2022-07-04 02:09:33 +0800

    Added Model classes.

--- database/migrations/2022_06_30_095354_create_addon_reservation_table.php
+++ database/migrations/2022_06_30_095354_create_addon_reservation_table.php
@@ -1,5 +1,7 @@
 <?php
 
+use App\Models\Addon;
+use App\Models\Reservation;
 use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
